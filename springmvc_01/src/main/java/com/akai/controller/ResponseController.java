6f111ee2d8e73f7db19720277257ab4d9023ccdd
loveshiho <1822881521@qq.com>
2024-04-23 16:51:17 +0800

    测试mvc——>转发和重定向——>Servlet实现

--- springmvc_01/src/main/java/com/akai/controller/ResponseController.java
+++ springmvc_01/src/main/java/com/akai/controller/ResponseController.java
@@ -1,7 +1,12 @@
 package com.akai.controller;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
 
 @Controller
 public class ResponseController {
@@ -9,5 +14,23 @@ public class ResponseController {
     public void testVoid() {
         System.out.println("void controller");
         // void controller,实际上是执行了,只不过是要跳转的资源没找到,或者说不知道往哪跳,所以是会报错的
+    }
+    @RequestMapping("/testForWard")
+    public void testForWard(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            req.getRequestDispatcher("forward.jsp").forward(req, resp);
+        } catch (ServletException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+    }
+    @RequestMapping("/testRedirect")
+    public void testRedirect(HttpServletResponse resp) {
+        try {
+            resp.sendRedirect("redirect.jsp");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ springmvc_01/src/main/webapp/forward.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhong
+  Date: 2024/4/23
+  Time: 16:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    this is forward page
+</body>
+</html>
