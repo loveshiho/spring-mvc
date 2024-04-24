cca445f73d488622aff29802684d9e599a158de8
loveshiho <1822881521@qq.com>
2024-04-24 13:20:47 +0800

    测试mvc——>作用域传参——>ModelAndView传递数据

--- springmvc_02/src/main/java/com/akai/controller/DataController.java
+++ springmvc_02/src/main/java/com/akai/controller/DataController.java
@@ -9,13 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class DataController {
     @Autowired
     private UserService userService;
+
     /*定义一个处理单元，向三个域中放入数据*/
     /*
      * request,session 这两个域直接放在参数列表上即可,SpringMVC就可以给我们注入
@@ -36,6 +39,7 @@ public class DataController {
         application.setAttribute("users", all);
         return "show.jsp";
     }
+
     /*model对象addAttribute
      * 主要是对请求域传递数据进行了API上的封装
      * 降低controller和Servlet之间的耦合度
@@ -49,5 +53,23 @@ public class DataController {
         model.addAttribute("msg", "halo req");
         model.addAttribute("users", all);
         return "show.jsp";
+    }
+
+    /*
+     * ModelAndView
+     * Model数据
+     * View 视图
+     * */
+    @RequestMapping("testMV")
+    public ModelAndView testMV() {
+        ModelAndView mv = new ModelAndView();
+        Map<String, Object> model = mv.getModel();  // 只是名字叫model,和Model对象并无关系
+        // 向request域中放入数据
+        List<User> users = userService.findAll();
+        model.put("msg", "halo req");
+        model.put("users", users);
+        // 设置视图
+        mv.setViewName("show.jsp");
+        return mv;
     }
 }
